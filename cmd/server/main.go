4558d2806c6abfc9b6e96a7c69059a338faaad95
hzx <hzx@test.com>
2025-07-21 14:01:48 +0800

    swagger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,7 +10,7 @@
 // @license.name MIT
 // @license.url https://opensource.org/licenses/MIT
 
-// @host localhost:8080
+// @host api-dev.718ai.cn
 // @BasePath /
 
 // @securityDefinitions.apikey BearerAuth
