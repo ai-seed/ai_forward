2157b50ebb75f18aead3a826d43f9d7d4ef86f9c
hzx <hzx@test.com>
2025-07-23 16:56:43 +0800

    删除没用的东西

--- migrations/003_create_tools_tables.down.sql
+++
@@ -1,5 +0,0 @@
--- 删除工具相关表
-
-DROP TABLE IF EXISTS tool_usage_logs;
-DROP TABLE IF EXISTS user_tool_instances;
-DROP TABLE IF EXISTS tools;
--- migrations/005_add_table_comments.down.sql
+++
@@ -1,4 +0,0 @@
--- 回滚表注释
-
--- 删除注释表
-DROP TABLE IF EXISTS table_comments;
